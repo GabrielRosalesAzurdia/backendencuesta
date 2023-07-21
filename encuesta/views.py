c851da51fdac4affe494d9b523b2609a32ccbe17
Gabriel Rosales <gabrielalejandrorosalesazurdia@gmail.com>
2023-07-21 10:13:50 -0600

    agregado rest respuesta

--- encuesta/views.py
+++ encuesta/views.py
@@ -3,11 +3,15 @@ from .serializer import AnswerSerializer
 from .models import Answer
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import json
 
 class AnswerViewSet(viewsets.ModelViewSet):
     queryset = Answer.objects.all()
     serializer_class = AnswerSerializer
 
+@api_view(['GET', 'POST'])
 def darResultados(request):
     queryset = Answer.objects.all()
     respuestas = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
@@ -21,5 +25,5 @@ def darResultados(request):
                 respuestas[i-1][1] = respuestas[i-1][1] + 1
             if(actual == 3):
                 respuestas[i-1][2] = respuestas[i-1][2] + 1
-    return HttpResponse(respuestas)
+    return Response(json.dumps(respuestas))
         
